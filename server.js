2fe53ea9c7a10e29e4e01f8936ba13583b544261
Esp-32-button <unknownentity107@gmail.com>
2025-02-01 17:58:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -110,6 +110,17 @@ app.post('/change_wifi', authenticateToken, async (req, res) => {
     }
 });*/
 
+function authenticateToken(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: "Forbidden" });
+    req.user = user;
+    next();
+  });
+}
+
 let servoState = "OFF"; // Default state
 
 app.post("/servo", authenticateToken, (req, res) => {
